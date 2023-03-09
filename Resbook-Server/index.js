d785d9dd896cfd150b6c530ff12b4f8b85088409
Sampath <81680345+Sampath-18@users.noreply.github.com>
2023-03-09 19:59:54 +0530

    Database path corrected

--- Resbook-Server/index.js
+++ Resbook-Server/index.js
@@ -1,120 +1,74 @@
-const express=require('express')
-require('dotenv').config()
-require('./db/conn')
-const User=require("./models/user");
-console.log(User)
+const express = require("express");
+require("dotenv").config();
+require("./db/conn");
+const User = require("./models/user");
+console.log(User);
 // const  cors=require('cors')
-const app=express()
+const app = express();
 
 // const auth=require('./routers/auth.js')
 
-
-
- 
- 
-
-const port=process.env.REACT_APP_PORT
-console.log(process.env.REACT_APP_PORT);
+const port = process.env.REACT_APP_PORT;
+// console.log(process.env.REACT_APP_PORT);
 // app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // app.use('/auth',auth)
 
+app.get("/", (req, res) => {
+  res.send("<h1>namaste</h1>");
+});
 
-  
-
-app.get('/',(req,res)=>{
-    res.send('<h1>namaste</h1>')
-})
-
-
-
-
-
-//app.post("/students",(req,res)=>{
-   // console.log(req.body);
-    
-   
-  // user.save().then(()=>{
-      
-   //}).catch((e)=>{
-      
-  // })
-    
-//})
-app.post("/signup",async(req,res)=>{
-    const {fname,lname,email,password,cpassword}=req.body
-try{
-    if(!fname||!lname||!email||!password||!cpassword)
-    {
-        return res.status(422).json({error:"please fill the field property"})
+app.post("/signup", async (req, res) => {
+  const { fname, lname, email, password, cpassword } = req.body;
+  try {
+    if (!fname || !lname || !email || !password || !cpassword) {
+      return res.status(422).json({ error: "please fill the field property" });
     }
     if (password != cpassword) {
-        return res.status(422).json({error:"password do not match"})
-      }
-
-      else{
-        User.findOne({ email: email }).then(user=>{
-            if (user) {
-                return res.status(422).json({error:"Email already exists"})
-            }
-            else {
-                const user=new User({fname,lname,email,password,cpassword});
-                 user.save().then(()=>{
-                    return res.status(201).json({message:"user created successfully"})
-
-                }).catch((err)=>{
-                    res.status(500).json({error:'failed to register'})
-                })
+      return res.status(422).json({ error: "password do not match" });
+    } else {
+      User.findOne({ email: email }).then((user) => {
+        if (user) {
+          return res.status(422).json({ error: "Email already exists" });
+        } else {
+          const user = new User({ fname, lname, email, password, cpassword });
+          user
+            .save()
+            .then(() => {
+              return res
+                .status(201)
+                .json({ message: "user created successfully" });
+            })
+            .catch((err) => {
+              res.status(500).json({ error: "failed to register" });
+            });
+        }
+      });
+    }
+  } catch (e) {
+    res.status(400).send(e);
   }
-        }) 
-          
-       
-    
+});
 
-}
-}
-catch(e){
-    res.status(400).send(e)
-}
-    
-})
-
-
-app.post("/login",async(req,res)=>{
-    const{email,password}=req.body
-    if(!email||!password)
-    {res.status(400).json({err:"please fill the above details"})}
-    try{
-        const userLogin= await User.findOne({email:email})
-        // console.log(userLogin.password);
-        // console.log(typeof(userLogin.password));
-        // console.log(password);
-        if(userLogin.password==password){
-            res.status(201).json({message:"User login successfully"})
-
-        }
-        else(!userLogin)
-        {
-            res.status(400).json({error:'user error'})
-        }
-       
-
-          
-          
-    
-             
-          
-
-
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ err: "please fill the above details" });
+  }
+  try {
+    const userLogin = await User.findOne({ email: email });
+    if (userLogin.password == password) {
+      res.status(201).json({ message: "User login successfully" });
+    } else !userLogin;
+    {
+      res.status(400).json({ error: "user error" });
     }
-    catch(err){
-        console.log(err);
-    }
-    
-})
- 
-app.listen(port,()=>{
-console.log(`server is connected to ${port}`);
-})
+  } catch (err) {
+    console.log(err);
+  }
+});
 
+app.listen(port, () => {
+  console.log(`server is connected to ${port}`);
+});
--- Resbook-Server/models/user.js
+++ Resbook-Server/models/user.js
@@ -1,36 +1,26 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const UserSchema=new mongoose.Schema({
-    fname:{
-        type:String,
-        required:true,
-      
-    },
-    lname:{
-        type:String,
-        required:true,
-       
-      
-    },
-    email:{
-        type:String,
-        required:true,
-      
-     
-},
-password:{
-    type:String,
-   
-    required:true,
-    
-
-
-},
-cpassword:{
-    type:String,
-    required:true,
-}
-      
+const UserSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cpassword: {
+    type: String,
+    required: true,
+  },
 });
-const User=new mongoose.model("User",UserSchema);
-module.exports=User;
+const User = new mongoose.model("Users", UserSchema, "Users");
+module.exports = User;
